5088cde175440921161d1250b626b3efa8d071a1
Shunji Zhan <shunhahaha@live.com>
2023-06-07 15:27:39 +0800

    tutorial v2

--- echo/test/echo.js
+++ echo/test/echo.js
@@ -1,10 +1,5 @@
 const Echo = artifacts.require('Echo');
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
 contract('Echo', function (/* accounts */) {
   let instance;
 
--- token/test/token.js
+++ token/test/token.js
@@ -2,11 +2,6 @@ const Token = artifacts.require('Token');
 const truffleAssert = require('truffle-assertions');
 const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
 contract('Token', function (accounts) {
   let instance;
   let deployer;
